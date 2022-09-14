cb67650bd07172c3e36a35db59f82772ac084938
Kalibek <matrix2006-06@mail.ru>
2022-09-14 10:27:13 +0600

    14/09/2022 10:26 ч

---
+++ src/main/java/kz/kk/spring_prob1/TestBean.java
@@ -0,0 +1,17 @@
+package kz.kk.spring_prob1;
+
+public class TestBean {
+    private String name;
+
+    public TestBean(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
